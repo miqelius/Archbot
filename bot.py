c5e3110f2a6856eea179e338941472f980b54c00
Giorgi Mikeladze <_@gmail.com>
2026-09-04 09:59:37 +0200

    fix: clean bot.py with dynamic routers

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, Router
 from core.core_config import settings
+import importlib
+import pkgutil
+import routers
 
 bot = Bot(token=settings.telegram_bot_token)
 dp = Dispatcher()
+
+for _, module_name, _ in pkgutil.iter_modules(routers.__path__):
+    module = importlib.import_module(f"routers.{module_name}")
+    if hasattr(module, "router") and isinstance(module.router, Router):
+        dp.include_router(module.router)
